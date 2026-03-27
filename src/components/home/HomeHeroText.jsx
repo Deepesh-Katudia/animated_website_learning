7820c6d4e968939c6459e93f9c5a614ebcbf6aee
Deepesh-Katudia <deepeshkatudia6201@gmail.com>
2026-03-26 23:20:32 -0400

    feat: update HomeHeroText component and add font styles in index.css

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -1,8 +1,13 @@
 import React from 'react'
+import Video from './Video'
 
 const HomeHeroText = () => {
   return (
-    <div>HomeHeroText</div>
+    <div className='font-[font1] pt-5 text-center ' >
+      <div className='text-[5vw] uppercase leading-[5vw] justify-center flex items-center' >Hi! I am Deepesh Katudia</div>
+      <div className='text-[4vw] uppercase leading-[5vw] justify-center flex items-start' >Building Scalable <div className='h-[4vw] rounded-full overflow-hidden'><Video /></div> Full-Stack Systems</div>
+      <div className='text-[5vw] uppercase leading-[5vw] justify-center flex items-center' >powered by intelligent Systems and Real-world impact</div>
+    </div>
   )
 }
 
