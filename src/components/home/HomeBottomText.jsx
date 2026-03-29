d24f4336b37976e0295ed200cac9ff4dc7096d7a
Deepesh-Katudia <deepeshkatudia6201@gmail.com>
2026-03-28 20:22:14 -0400

    feat: update HomeBottomText component layout and enhance Home page structure

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -1,8 +1,12 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div>HomeBottomText</div>
+    <div className='font-[font2] flex items-center justify-center gap-8'>
+      <Link to='/projects' className='border-4 leading-[6vw] border-white rounded-full text-[5.5vw] px-8' > Projects </Link>
+      <Link to='/contact' className='border-4 leading-[6vw] border-white rounded-full text-[5.5vw] px-8' > Contact </Link>
+    </div>
   )
 }
 
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -7,7 +7,7 @@ export const Home = () => {
   return (
     <div>
       <div className='h-screen w-screen fixed'>  <Video /> </div>   
-        <div className='h-screen w-screen relative flex flex-col'>
+        <div className='h-screen w-screen relative flex flex-col justify-between'>
             <HomeHeroText />
             <HomeBottomText />
         </div>
