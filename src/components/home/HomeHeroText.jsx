429b3575dd0d6e82c8ff2c12e8f0e8cdc52d9440
Deepesh-Katudia <deepeshkatudia6201@gmail.com>
2026-03-28 20:22:19 -0400

    feat: adjust Video component size in HomeHeroText for better layout

--- src/components/home/HomeHeroText.jsx
+++ src/components/home/HomeHeroText.jsx
@@ -5,7 +5,7 @@ const HomeHeroText = () => {
   return (
     <div className='font-[font1] pt-5 text-center ' >
       <div className='text-[5vw] uppercase leading-[5vw] justify-center flex items-center' >Hi! I am Deepesh Katudia</div>
-      <div className='text-[4vw] uppercase leading-[5vw] justify-center flex items-start' >Building Scalable <div className='h-[4vw] rounded-full overflow-hidden'><Video /></div> Full-Stack Systems</div>
+      <div className='text-[4vw] uppercase leading-[5vw] justify-center flex items-start' >Building Scalable <div className='h-[4.5vw] w-[12vw] rounded-full overflow-hidden'><Video /></div> Full-Stack Systems</div>
       <div className='text-[5vw] uppercase leading-[5vw] justify-center flex items-center' >powered by intelligent Systems and Real-world impact</div>
     </div>
   )
