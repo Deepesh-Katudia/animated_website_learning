1eaf86274df666bbfdd70aae48649975311da8fa
Deepesh-Katudia <deepeshkatudia6201@gmail.com>
2026-04-08 21:45:38 -0400

    feat: update Contact component layout and add styling elements

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -2,7 +2,19 @@ import React from 'react'
 
 const Contact = () => {
   return (
-    <div>Contact</div>
+<div>
+  <div  className='h-[20vw] rounded-3xl left-[30vw] w-[15vw] top-50 absolute bg-red-50'>
+    <img src="/animated_website/public/home-video.mp4" alt="" />
+  </div>
+      <div className='font-[font2]'>
+      <div className='mt-[50vh]'>
+        <h1 className='text-[15vw] text-center uppercase leading-[15vw]'>Contact</h1>
+      </div>
+      <div className='pl-[50%] mt-20' >
+        <p className='text-6xl w-1/2'>&nbsp;Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsum sint quasi quos possimus facilis delectus magnam iusto culpa ab dolores.</p>
+      </div>
+    </div>
+</div>
   )
 }
 
