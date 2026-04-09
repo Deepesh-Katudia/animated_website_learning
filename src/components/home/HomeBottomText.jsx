555b7e1a70e1b1567344687469ff6d6927a79c89
Deepesh-Katudia <deepeshkatudia6201@gmail.com>
2026-04-08 21:45:33 -0400

    feat: enhance layout and styling of HomeBottomText component

--- src/components/home/HomeBottomText.jsx
+++ src/components/home/HomeBottomText.jsx
@@ -3,9 +3,13 @@ import { Link } from 'react-router-dom'
 
 const HomeBottomText = () => {
   return (
-    <div className='font-[font2] flex items-center justify-center gap-8'>
-      <Link to='/projects' className='border-4 leading-[6vw] border-white rounded-full text-[5.5vw] px-8' > Projects </Link>
-      <Link to='/contact' className='border-4 leading-[6vw] border-white rounded-full text-[5.5vw] px-8' > Contact </Link>
+    <div className="font-[font2] flex items-center justify-center gap-2">
+    <div className='border-4 leading-[6vw] border-white rounded-full text-[5.5vw] px-10 mb-2 hover:border-[#D3FD50] hover:text-[#D3FD50]'>
+      <Link to='/projects' className='text-[6vw] mt-6'  > Projects </Link>
+      </div>
+      <div  className='border-4 leading-[6vw] border-white rounded-full text-[5.5vw] px-10 mb-2 hover:border-[#D3FD50] hover:text-[#D3FD50]'>
+      <Link to='/contact' className='text-[6vw] mt-6' > Contact </Link>
+    </div>
     </div>
   )
 }
